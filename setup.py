76fdb8a93475dccf457126122197c5bb56591a01
gulshansharm-a <102583501+gulshansharm-a@users.noreply.github.com>
2022-11-17 22:17:45 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,17 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
+
+VERSION = '0.0.13'
+DESCRIPTION = 'Streami'
+LONG_DESCRIPTION = 'A package that allows to build simple streams of video, audio and camera data.'
+
+# Setting up
+setup(
+    name="from setuptools import setup, find_packages
+import codecs
+import os
+
 here = os.path.abspath(os.path.dirname(__file__))
 
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
@@ -14,7 +25,25 @@ LONG_DESCRIPTION = 'A package that allows to build simple streams of video, audi
 
 # Setting up
 setup(
-    name="vidstream",
+    name="vasictestapp",
+    version=VERSION,
+    author="NeuralNine (Florian Dedov)",
+    author_email="<mail@neuralnine.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['opencv-python', 'pyautogui', 'pyaudio'],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)",
     version=VERSION,
     author="NeuralNine (Florian Dedov)",
     author_email="<mail@neuralnine.com>",
